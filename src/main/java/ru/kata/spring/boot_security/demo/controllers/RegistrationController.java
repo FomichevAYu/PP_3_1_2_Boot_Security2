b5ef567ceefaaaa7b965096135e2a97c7e7c5c48
FomichevAYu <mailforspoify@yandex.ru>
2023-05-27 16:06:27 +0300

    add newUserForm and controller

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RegistrationController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RegistrationController.java
@@ -1,9 +1,16 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.models.User;
 
 @Controller
 @RequestMapping("/reg")
 public class RegistrationController {
+    @GetMapping("/")
+    public String showNewUserPage(@ModelAttribute("user") User user) {
+        return "NewUserPage";
+    }
 }
